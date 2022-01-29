ebeeddbd723afde66c5de3ca6e198cda807415a2
Giovanni <gio.a.98@gmail.com>
2022-01-29 20:13:05 +0100

    create random boxes with debug button

--- src/script.js
+++ src/script.js
@@ -22,6 +22,20 @@ debugObject.createSphere = () => {
 }
 gui.add(debugObject, 'createSphere')
 
+debugObject.createBox = () => {
+    createBox(
+        Math.random(),
+        Math.random(),
+        Math.random(),
+        {
+            x: (Math.random() - .5) * 3,
+            y: 3,
+            z: (Math.random() - .5) * 3
+        }
+    )
+}
+gui.add(debugObject, 'createBox')
+
 /**
  * Base
  */
@@ -209,6 +223,7 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  */
 const objectsToUpdate = []
 
+// Sphere
 const sphereGeometry = new THREE.SphereBufferGeometry(1, 20, 20)
 const sphereMaterial = new THREE.MeshStandardMaterial({
     metalness: .3,
@@ -244,6 +259,42 @@ const createSphere = (radius, position) => {
 
 createSphere(.5, {x: 0, y: 3, z: 0})
 
+// Box
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
+const boxMaterial = new THREE.MeshStandardMaterial({
+    metalness: .3,
+    roughness: .4,
+    envMap: environmentMapTexture
+})
+
+const createBox = (width, height, depth, position) => {
+    // Three.js mesh
+    const mesh = new THREE.Mesh(boxGeometry, boxMaterial)
+    mesh.scale.set(width, height, depth)
+    mesh.castShadow = true
+    mesh.position.copy(position)
+    scene.add(mesh)
+
+    // Cannon.js body
+    const shape = new CANNON.Box(new CANNON.Vec3(width * .5, height * .5, depth * .5))
+    const body = new CANNON.Body({
+        mass: 1,
+        position: new CANNON.Vec3(0, 4, 0),
+        shape,
+        material: defaultMaterial
+    })
+    body.position.copy(position)
+    world.addBody(body)
+
+    // Save in objects to update
+    objectsToUpdate.push({
+        mesh,
+        body
+    })
+}
+
+createBox(1, 1, 1, {x: 0, y: 4, z: 0})
+
 /**
  * Animate
  */
@@ -268,6 +319,7 @@ const tick = () =>
 
     for(const object of objectsToUpdate) {
         object.mesh.position.copy(object.body.position)
+        object.mesh.quaternion.copy(object.body.quaternion)
     }
 
     // Update controls
